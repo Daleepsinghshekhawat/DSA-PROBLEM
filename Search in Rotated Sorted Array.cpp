29a5d76d49453ed1b33caf8b396c517fafc19d0d
daleepsingh <ds9401841@gmail.com>
2026-01-11 22:22:38 +0530

    Search in Rotated Sorted Array solution

---
+++ Search in Rotated Sorted Array.cpp
@@ -0,0 +1,56 @@
+// File: search_in_rotated_sorted_array.cpp
+// Problem: Search in Rotated Sorted Array
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Left half is sorted
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+
+    int result = sol.search(nums, target);
+
+    if (result != -1)
+        cout << "Target found at index: " << result << endl;
+    else
+        cout << "Target not found" << endl;
+
+    return 0;
+}
